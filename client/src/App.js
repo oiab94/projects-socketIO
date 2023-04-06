3382cec779e8844705f3435864a35bc5149e71dc
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-06 18:13:54 -0400

    client: Permite seleccionar si se conecta o desconecta el socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import socket from "./socket";
+
 function App() {
+	const [isConnected, setIsConnected] = useState(false);
+
+	const connectSocket = () => {
+		console.log("Antes de conectar: ", socket);
+		socket.connect();
+		socket.on("connection", () => setIsConnected(true));
+	};
+
+	const disconnectSocket = () => {
+		console.log("Despues de conectar: ", socket);
+		socket.disconnect("disconnect", () => setIsConnected(false));
+	};
+
 	return (
 		<>
 			<h1>Hello World</h1>
+			<button onClick={ connectSocket }>Connect</button>
+			<button onClick={ disconnectSocket }>Disconnect</button>
+			{
+				isConnected ? 
+					<h2>Socket is ON</h2> :
+					<h2>Socket is OFF</h2>
+			}
 		</>
 	);
 }
