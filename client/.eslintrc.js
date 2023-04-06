c54e28273d1bda9a032a1675f4ae7bfd2a6fa3d7
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-06 15:14:57 -0400

    client: prueba de cliente

---
+++ client/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react"
+	],
+	"rules": {
+		"react/react-in-jsx-scope": "off",
+		"react/react-uses-react": "off",
+		"react/jsx-filename-extension": [
+			1, 
+			{ 
+				"extensions": [
+					".js", ".jsx", ".ts", ".tsx"
+				] 
+			}
+		],
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
