ed864818eaeb7b1d9a02c977b2243e30760951e6
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-06 18:15:31 -0400

    server: Configuraciones cors para el socket

--- server/server.js
+++ server/server.js
@@ -1,22 +1,35 @@
-import express from "express";
 import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import testRoute from "./routes/test.route";
 import { Server as WebSocketServer } from "socket.io";
 import http from "http";
 
 // * VARIABLES
-const app = express(); // Configuramos express
-const server = http.createServer(app); // Servimos las configuraciones de express a nuestro servidor, a partir de aqui la escucha al servidor se realiza con http
-const io = new WebSocketServer(server); // Creamos la conexion de sockets que utiliza nuestro servidor
+// Configuramos express
+const app = express();
+
+// Servimos las configuraciones de express a nuestro servidor, a partir de aqui la escucha al servidor se realiza con http
+const server = http.createServer(app);
+
+// Creamos la conexion a nuestro socket, además este tiene configuraciones cors para permitir que solo nuestro FrontEnd se conecte
+const io = new WebSocketServer(
+	server, {
+		cors : {
+			origin: process.env.URL,
+			credentials: true,
+		}
+	}
+); // Creamos la conexion de sockets que utiliza nuestro servidor
 
 // * CONFIGURACIONES
-dotenv.config();
 server.listen(
 	process.env.PORT,
 	() => {
 		console.log(`Express-> Se escucha en el puerto ${process.env.PORT}`);
 	}
 );
+
 io.on(
 	"connection",
 	() => {
