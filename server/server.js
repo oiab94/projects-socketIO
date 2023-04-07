e7d7c11d5fb840b04d3868fa89ec0831d2f5fd6a
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-07 10:17:34 -0400

    server: Refactor del código

--- server/server.js
+++ server/server.js
@@ -1,41 +1,33 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import testRoute from "./routes/test.route";
-import { Server as WebSocketServer } from "socket.io";
-import http from "http";
+import { connection } from "./controllers/io.controller";
 
 // * VARIABLES
-// Configuramos express
 const app = express();
-
-// Servimos las configuraciones de express a nuestro servidor, a partir de aqui la escucha al servidor se realiza con http
-const server = http.createServer(app);
-
-// Creamos la conexion a nuestro socket, además este tiene configuraciones cors para permitir que solo nuestro FrontEnd se conecte
-const io = new WebSocketServer(
-	server, {
-		cors : {
-			origin: process.env.URL,
-			credentials: true,
-		}
-	}
-); // Creamos la conexion de sockets que utiliza nuestro servidor
-
-// * CONFIGURACIONES
-server.listen(
-	process.env.PORT,
-	() => {
-		console.log(`EXPRESS: Se escucha en el puerto ${process.env.PORT}`);
-	}
+const httpServer = createServer(app);
+const corsOptions = {
+	origin:process.env.URL,
+	credentials:true,
+};
+const io = new Server(
+	httpServer,
+	{ cors: corsOptions }
 );
 
-io.on(
-	"connection",
-	(socket) => {
-		console.log(`SOCKET: Nueva conexion establecida con ${socket.id}`);
+// Ponemos a la escucha nuestro servidor con SocketIO y Express
+httpServer.listen(
+	process.env.PORT,
+	() => {
+		console.log(`SERVER: Se escucha en el puerto ${process.env.PORT}`);
 	}
 );
 
 // * RUTAS
 app.use(testRoute);
+
+// * SOCKET
+connection(io);
