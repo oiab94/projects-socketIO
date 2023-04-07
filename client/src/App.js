3dd5d30f1b46ece7ee51624a8ea9e29dfe2df640
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-06 22:01:14 -0400

    client: Implementa home

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
-import socket from "./socket";
+import { Home } from "./views/Home";
 
 function App() {
-	const [isConnected, setIsConnected] = useState(false);
-
-	const connectSocket = () => {
-		console.log("Antes de conectar: ", socket);
-		socket.connect();
-		socket.on("connection", () => setIsConnected(true));
-	};
-
-	const disconnectSocket = () => {
-		console.log("Despues de conectar: ", socket);
-		socket.disconnect("disconnect", () => setIsConnected(false));
-	};
-
 	return (
 		<>
-			<h1>Hello World</h1>
-			<button onClick={connectSocket}>Connect</button>
-			<button onClick={disconnectSocket}>Disconnect</button>
-			{isConnected ? <h2>Socket is ON</h2> : <h2>Socket is OFF</h2>}
+			<Home />
 		</>
 	);
 }
