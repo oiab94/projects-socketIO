bad476030597861fd58307f0eb2583923bf55595
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-07 15:31:59 -0400

    client: Recibimos los datos enviados del servidor

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const Chat = () => {
+	// Obtenemos los chats
+	const fetchChats = async () => {
+		const { data } = await axios.get("/api/chat");
+		
+		console.log(data);
+	};
+
+	// Efectos
+	useEffect(() => {
+		fetchChats();
+	}, []);
+
 	return (
 		<>
 			<h1>Chat Page</h1>
