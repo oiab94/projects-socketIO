8f79bec74ed2b18f810ac77fd51adb7854ce2ca2
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-07 19:25:04 -0400

    client: Botones de acceso a la página

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import { ThemeProvider, Container, Row, Col, Button } from "react-bootstrap";
+
 const Home = () => {
+	const [isSelected, setIsSelected] = useState(true);
+
 	return (
-		<>
-			<h1>Home Page</h1>
-		</>
+		<ThemeProvider breakpoints={["xl", "lg", "md"]} minBreakpoint="md">
+			<Container>
+				<Row className="mt-3 border rounded">
+					<Col className="d-flex justify-content-center">
+						<h1 className="fs">Chat App</h1>
+					</Col>
+				</Row>
+
+				<Row className="mt-3 border rounded">
+					<Row className="justify-content-center p-3">
+						<Col
+							md={ 5 }
+							as={ Button }
+							active={ isSelected }
+							onClick={ () => setIsSelected(!isSelected) }
+							variant="outline-dark"
+							className="rounded-pill me-3" >
+							Login
+						</Col>
+						<Col
+							md={ 5 }
+							as={ Button }
+							active={ !isSelected }
+							onClick={ () => setIsSelected(!isSelected) }
+							variant="outline-dark"
+							className="rounded-pill" >
+							Sign up
+						</Col>
+					</Row>
+				</Row>
+			</Container>
+		</ThemeProvider>
 	);
 };
 
