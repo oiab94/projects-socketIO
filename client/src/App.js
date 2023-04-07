18d61df6a83d65e46c716caf606c3e1707b45dcf
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-07 15:13:26 -0400

    client: Ahora se usa App como el router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./views/Home";
+import Chat from "./views/Chat";
 
 function App() {
-	return (
-		<>
-		</>
-	);
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path:"/chats",
+			element: <Chat />,
+		}
+	]);
+
+	return <RouterProvider router={router} />;
 }
 
 export default App;
