bad6210b3034e5fc2ece4ad0fefea104669a0a64
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-09 15:40:42 -0400

    server: Cambio de exportacion de modulo

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
 	name: { type: String, required: true },
-	email: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	picture: {
 		type: String,
@@ -14,6 +14,4 @@ const userSchema = mongoose.Schema({
 
 const UserModel = mongoose.model("User", userSchema);
 
-module.exports = {
-	UserModel,
-};
+module.exports = UserModel;
