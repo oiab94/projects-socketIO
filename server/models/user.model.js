77d63f4df334629703d9e5ebee1ea3d602b56283
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-09 16:09:14 -0400

    server: Login y encriptacion de password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
 	name: { type: String, required: true },
@@ -11,6 +12,17 @@ const userSchema = mongoose.Schema({
 			"https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png",
 	},
 },{ timestamps: true });
+
+userSchema.pre("save", async function (next) {
+	if(!this.isModified) next();
+	
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const UserModel = mongoose.model("User", userSchema);
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
-const { registerUser } = require("../controllers/user.controllers");
+const { registerUser, authUser } = require("../controllers/user.controllers");
 
 // * Variables
 const router = Router();
 
 // * Direccionamiento
 router.post("/signup", registerUser);
-// router.post("/login", authUser);
+router.post("/login", authUser);
 
 module.exports = router;
