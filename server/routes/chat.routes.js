8a018a12297097f96d54c3e93c0aaacbecf35cda
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-11 12:11:21 -0400

    server: ruta de acceso a chat_gets

--- server/routes/chat.routes.js
+++ server/routes/chat.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { protect } = require("../middleware/auth.middleware");
-const { chat_access } = require("../controllers/chat.controllers");
+const { chat_access, chat_get } = require("../controllers/chat.controllers");
 
 // * Variables
 const router = Router();
@@ -8,7 +8,7 @@ const router = Router();
 // * Direccionamiento
 router.post("/", protect, chat_access);
 // router.post("/group", protect, group_create);
-// router.get("/", protect, chat_get);
+router.get("/", protect, chat_get);
 // router.put("/groupRename", protect, group_rename);
 // router.put("/groupRemove", private, group_removeFrom);
 // router.put("/groupAdd", private, group_addTo);
