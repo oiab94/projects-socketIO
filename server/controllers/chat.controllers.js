06f237cb6e8bcdcd2ad22a07cbd5ce9092839f75
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-11 11:41:53 -0400

    server: Acceso a chat 1 a 1

---
+++ server/controllers/chat.controllers.js
@@ -0,0 +1,54 @@
+const asyncHandlers = require("express-async-handler");
+const { Chat } = require("../models/chat.model");
+const User = require("../models/user.model");
+
+const chat_access = asyncHandlers(async (req, res) => {
+	const { userId } = req.body;
+
+	if(!userId){
+		console.log("UserID param not sent with request");
+		return res.status(400);
+	}
+
+	let isChat = await Chat.find({
+		isGroupChat: false,
+		// Como $or ahora $and debe ser ambos ciertos
+		$and: [
+			{ users: { $elemMatch: {$eq: req.user._id} } },
+			{ users: { $elemMatch: {$eq: userId} } }
+		]
+	})
+		.populate("users", "-password")
+		.populate("latestMessage");
+
+	isChat = await User.populate(isChat, {
+		path:"latestMessage.sender",
+		select:"name picture email",
+	});
+
+	if(isChat.length > 0){
+		res.send(isChat[0]);
+	} else {
+		let chatData = {
+			chatName: "sender",
+			isGroupChat: false,
+			users: [req.user._id, userId]
+		};
+
+		try{
+			const createdChat = await Chat.create(chatData);
+			const FullChat = await Chat.findOne({
+				_id: createdChat._id
+			}).populate("users", "-password");
+
+			res.status(200).send(FullChat);
+		}catch(error){
+			res.status(400);
+			throw new Error(error.message);
+		}
+	}
+});
+
+module.exports = {
+	chat_access,
+};
