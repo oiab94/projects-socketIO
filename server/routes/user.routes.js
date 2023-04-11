48a097eae84162ec99eba9b3deff39e292570ce7
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-11 10:50:24 -0400

    server: Ruta de acceso a getUsers

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
-const { registerUser, authUser } = require("../controllers/user.controllers");
+const { registerUser, authUser, allUsers } = require("../controllers/user.controllers");
+const { protect } = require("../middleware/auth.middleware");
 
 // * Variables
 const router = Router();
@@ -7,5 +8,6 @@ const router = Router();
 // * Direccionamiento
 router.post("/signup", registerUser);
 router.post("/login", authUser);
+router.get("/getUsers", protect, allUsers);
 
 module.exports = router;
