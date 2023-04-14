26aabdcbccfa75830f616be2c4c44aa85e4316e5
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-13 21:22:26 -0400

    server: Creación de un grupo

--- server/controllers/chat.controllers.js
+++ server/controllers/chat.controllers.js
@@ -70,7 +70,41 @@ const chat_get = asyncHandlers(async (req, res) => {
 	}
 });
 
+const group_create = asyncHandlers(async (req, res) => {
+	// Chequeamos el request
+	if(!req.body.users || !req.body.name)
+		return res.status(400).send({message: "Please fill all the fields"});
+
+	let users = JSON.parse(req.body.users);
+
+	if(users.length < 2)
+		return res.status(400).send("More than 2 users are required to form a group chat");
+
+	// Agregamos nuestro usuario tambien al grupo
+	users.push(req.user);
+
+	// Creamos en nuestra BD
+	try {
+		const groupChat = await Chat.create({
+			chatName: req.body.name,
+			users: users,
+			isGroupChat: true,
+			groupAdmin: req.user
+		});
+
+		const fullGroupChat = await Chat.findOne({_id: groupChat._id})
+			.populate("users", "-password")
+			.populate("groupAdmin", "-password");
+
+		res.status(200).json(fullGroupChat);
+	} catch (error) {
+		res.status(400);
+		throw new Error(error.message);
+	}
+});
+
 module.exports = {
 	chat_access,
-	chat_get
+	chat_get,
+	group_create
 };
