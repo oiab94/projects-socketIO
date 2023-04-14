b0a82d66e09f1a02cd4d0c3f189c1f89f29ad77c
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-13 21:55:21 -0400

    serves: Ruta de acceso a la api

--- server/routes/chat.routes.js
+++ server/routes/chat.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { protect } = require("../middleware/auth.middleware");
-const { chat_access, chat_get, group_create, group_rename } = require("../controllers/chat.controllers");
+const { chat_access, chat_get, group_create, group_rename, group_addTo, group_removeTo } = require("../controllers/chat.controllers");
 
 // * Variables
 const router = Router();
@@ -10,7 +10,7 @@ router.post("/", protect, chat_access);
 router.post("/groupCreate", protect, group_create);
 router.get("/", protect, chat_get);
 router.put("/groupRename", protect, group_rename);
-// router.put("/groupRemove", private, group_removeFrom);
-// router.put("/groupAdd", private, group_addTo);
+router.put("/groupAddTo", protect, group_addTo);
+router.put("/groupRemoveTo", protect, group_removeTo);
 
 module.exports = router;
