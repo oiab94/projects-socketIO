8d21eff517e5394fe787c01dcc63174d59af02da
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-23 12:56:42 -0400

    client: Lista un usuario en nuestra tarjet

---
+++ client/src/components/UserAvatar/UserListItem.jsx
@@ -0,0 +1,30 @@
+import { Container, Col, Row, Image, Button } from "react-bootstrap";
+
+const UserListItem = ({ user, handleFunction }) => {
+
+	return (
+		<div className="pt-2">
+			<Container
+				className="border border-2 d-flex w-100 p-0 search-stack"
+				as={Button}
+			>
+				<Col onClick={handleFunction} xs={3} className="w-auto">
+					<Image src={user.picture} width="50px" height="50px" roundedCircle />
+				</Col>
+				<Col xs={9}>
+					<Row className="text-start">
+						<p className="lh-sm m-0 fw-light">
+							<span className="fs-5">{user.name}</span>
+							<br />
+							<span>
+								<b>Email:</b> {user.email}
+							</span>
+						</p>
+					</Row>
+				</Col>
+			</Container>
+		</div>
+	);
+};
+
+export default UserListItem;
