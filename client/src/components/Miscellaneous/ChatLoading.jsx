cf12f2055d4f79dca167a1896f1201effa062db3
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-24 21:40:30 -0400

    client: Modificación de chat loading

--- client/src/components/Miscellaneous/ChatLoading.jsx
+++ client/src/components/Miscellaneous/ChatLoading.jsx
@@ -4,9 +4,18 @@ const ChatLoading = () => {
 
 	return (
 		<Stack gap={2} className="pt-2">
-			<div className="border border-2 d-flex w-100 p-0 search-stack"></div>
-			<div className="border border-2 d-flex w-100 p-0 search-stack"></div>
-			<div className="border border-2 d-flex w-100 p-0 search-stack"></div>
+			<div
+				className="border border-2 d-flex w-100 p-0 search-stack"
+				style={{ height: "20px" }}
+			/>
+			<div
+				className="border border-2 d-flex w-100 p-0 search-stack"
+				style={{ height: "20px" }}
+			/>
+			<div
+				className="border border-2 d-flex w-100 p-0 search-stack"
+				style={{ height: "20px" }}
+			/>
 		</Stack>
 	);
 };
