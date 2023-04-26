4eff9c68dd927e2d813d6e8334ad93c96f747415
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-26 15:56:07 -0400

    client: Despliega la ventana de chat

--- client/src/components/Miscellaneous/ChatBox.jsx
+++ client/src/components/Miscellaneous/ChatBox.jsx
@@ -1,8 +1,22 @@
-const ChatBox = () => {
+import { ChatState } from "../../context/ChatProvider";
+import { Col, Row, Container } from "react-bootstrap";
+import { SingleChat } from "./SingleChat";
+
+const ChatBox = ({ fetchAgain, setFetchAgain }) => {
+	/*eslint-disable no-unused-vars */
+	const { selectedChat } = ChatState();
+
 	return (
-		<p>
-			chat box
-		</p>
+		<Container className="bg-white border rounded h-100">
+			<Row className="h-100">
+				<Col
+					className="d-flex justify-content-center"
+					style={{ color: "var(--bs-gray-dark)" }}
+				>
+					<SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 
