baac2d93dfc90b7d90420452a0e87ab8e71ae3d6
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-26 16:43:33 -0400

    client: Cambia el tamaño del email

--- client/src/components/Miscellaneous/ProfileModal.jsx
+++ client/src/components/Miscellaneous/ProfileModal.jsx
@@ -13,35 +13,31 @@ const ProfileModal = ({ children, user }) => {
 
 	return (
 		<div>
-			{
-				children ? (
-					<span onClick={ handleShow }>{ children }</span>
-				) : (
-					<Button variant="outline-secondary" onClick={() => setShow(true)}>
-						<FontAwesomeIcon icon={faEye} />
-					</Button>
-				)
-			}
-			<Modal 
-				centered
-				show={show} 
-				onHide={handleClose}>
+			{children ? (
+				<span onClick={handleShow}>{children}</span>
+			) : (
+				<Button variant="outline-secondary" onClick={() => setShow(true)}>
+					<FontAwesomeIcon icon={faEye} />
+				</Button>
+			)}
+			<Modal centered show={show} onHide={handleClose}>
 				<Modal.Header className="justify-content-center">
-					<Modal.Title>{ user.name }</Modal.Title>
+					<Modal.Title>{user.name}</Modal.Title>
 				</Modal.Header>
 				<Modal.Body className="d-flex flex-column align-items-center">
-					<Image 
-						src={ user.picture }
-						alt={ user.name }
+					<Image
+						src={user.picture}
+						alt={user.name}
 						width="150px"
 						height="150px"
-						roundedCircle />
-					<div className="fs-1">Email: { user.email }</div>
+						roundedCircle
+					/>
+					<div className="fs-1">
+						Email: <span className="fs-2">{user.email}</span>
+					</div>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button 
-						variant="secondary" 
-						onClick={handleClose}>
+					<Button variant="secondary" onClick={handleClose}>
 						Close
 					</Button>
 				</Modal.Footer>
