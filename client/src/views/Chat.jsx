90a3c80e885ccedbb75d044e3ceb8b1aabd89b97
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-04-26 15:57:03 -0400

    client: Llama a ChatBox

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -3,21 +3,23 @@ import { Container, Row, Col } from "react-bootstrap";
 import SideBar from "../components/Miscellaneous/SideBar";
 import MyChats from "../components/Miscellaneous/MyChats";
 import ChatBox from "../components/Miscellaneous/ChatBox";
+import { useState } from "react";
 
 const Chat = () => {
 	const { user } = ChatState();
+	const [fetchAgain, setFetchAgain] = useState(false);
+
 
 	return (
 		<>
 			<Container>
 				<Row>
-					<Col>{ user && <SideBar /> }</Col>
+					<Col>{user && <SideBar />}</Col>
 				</Row>
-				
+
 				<Row className="mt-3 mb-3 h-75">
-					<Col 
-						xs={ 5 } >{ user && <MyChats /> }</Col>
-					<Col>{ user && <ChatBox /> }</Col>
+					<Col xs={5}>{user && <MyChats fetchAgain={fetchAgain} />}</Col>
+					<Col>{user && <ChatBox fetchAgain={ fetchAgain } setFetchAgain={ setFetchAgain } />}</Col>
 				</Row>
 			</Container>
 		</>
